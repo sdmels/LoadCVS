61caf7888edd2cd04d9539c9a59db59324c6f19d
Daniel Susaimanickam <Daniel.Susaimanickam@sandstone.com.au>
2015-08-24 23:38:38 +1000

    view files are added

--- app/app.js
+++ app/app.js
@@ -3,8 +3,14 @@ define(['angular', 'appCtrl'],
     'use strict';
 
     // Define 'apply' module.
-    var app = angular.module('apply', []);
+    var app = angular.module('apply', ['angularUiRouter']);
     app.controller('appCtrl', appCtrl);
+
+    app.config([function () {
+
+    }]);
+
+
 
     return app;
   });
